5756452cfd2ed96bb7fc1a43c9dca55a1b2356b2
LeanTrey <treymclean0003@gmail.com>
2023-06-24 02:00:58 -0400

    main

--- pp/src/components/Skills.js
+++ pp/src/components/Skills.js
@@ -28,6 +28,7 @@ export const Skills = () => {
   };
 
   return (
+    <div className='skill-con'>
     <section className="skill" id="skills">
         <div className="container">
             <div className="row">
@@ -59,5 +60,6 @@ export const Skills = () => {
         </div>
         <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
+    </div>
   )
 }
