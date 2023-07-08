67099de976d8bc86fbcaa5906056ab88cd8cb43e
LeanTrey <treymclean0003@gmail.com>
2023-07-07 21:06:46 -0400

    fixed background bug

--- pp/src/components/Banner.js
+++ pp/src/components/Banner.js
@@ -40,7 +40,8 @@ export const Banner = () => {
                 <Row className='align-items-center'>
                     <Col xs={12} md={6} xl={7} >
                         <span className='tagline wf'>Welcome to my Portfolio</span>
-                        <h1 className="wf">{`Hi I'm Edward, `}<span className='wrap'>{text}</span></h1>
+                        <h1 className="wf banner-desc">
+                            <span className="cust-one">{`Hi I'm Edward, `}</span><span className='wrap cust-two'>{text}</span></h1>
                         <p>Self Taught WebDev Currently Doing Freelancing at UpWork.</p>
                         <button onClick={() => window.location.href="https://www.linkedin.com/in/edward-mclean-6073a1263/"}>Let's connect <ArrowRightCircle size={25}/></button>
                     </Col>
